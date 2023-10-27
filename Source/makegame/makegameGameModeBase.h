e31280c415fdd2257a49c1048b18a8b4312f6361
ajs <ajs18306016307@163.com>
2023-10-27 15:31:34 +0800

    makegame ue4 commit

---
+++ Source/makegame/makegameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "makegameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAKEGAME_API AmakegameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
